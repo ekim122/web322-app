34f641e0d9e4d56174e58aba32c50ded196af686
ekim122 <ekim122@myseneca.ca>
2022-02-02 01:37:02 -0500

    added 404.html and routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var blog = require("./blog-service.js");
 var app = express();
 
 var HTTP_PORT = process.env.PORT || 8080;
@@ -21,6 +22,27 @@ var path = require("path");
 app.get("/about", function(req,res){
     res.sendFile(path.join(__dirname,"/views/about.html"));
     //res.sendFile(app.join(__dirname, "/views/about.html"));
+});
+
+// setup another route to listen on /blog
+app.get("/blog", function(req,res){
+    //res.send(blog.join(__filename,"blog-service.js"));
+    res.send("TODO: get all posts who have published==true");
+});
+
+// setup another route to listen on /posts
+app.get("/posts", function(req,res){
+    res.send("TODO: get all posts within the posts.json file");
+});
+
+// setup another route to listen on /categories
+app.get("/categories", function(req,res){
+    res.send("TODO: get all posts within the categories.json file");
+});
+
+// setup another route to display 404
+app.use(function(req,res){
+    res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
 });
 
 // setup http server to listen on HTTP_PORT
