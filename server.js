66a534018b0a8387e96905be783370bcd0d2e072
ekim122 <ekim122@myseneca.ca>
2022-02-02 17:21:42 -0500

    updated server.js to get data from blog-service.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var blog = require("./blog-service.js");
+var blogService = require("./blog-service.js");
 var app = express();
 
 var HTTP_PORT = process.env.PORT || 8080;
@@ -27,17 +27,38 @@ app.get("/about", function(req,res){
 // setup another route to listen on /blog
 app.get("/blog", function(req,res){
     //res.send(blog.join(__filename,"blog-service.js"));
-    res.send("TODO: get all posts who have published==true");
+    //res.send("TODO: get all posts who have published==true");
+    blogService.getPublishedPosts().then((data)=>{
+        res.json(data)
+    }).catch((error)=>{
+        console.log(error)
+        //res.status(404).send("Error!")
+        res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+    })
 });
 
 // setup another route to listen on /posts
 app.get("/posts", function(req,res){
-    res.send("TODO: get all posts within the posts.json file");
+    //res.send("TODO: get all posts within the posts.json file");
+    blogService.getAllPosts().then((data)=>{
+        res.json(data)
+    }).catch((error)=>{
+        console.log(error)
+        //res.status(404).send("Error!")
+        res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+    })
 });
 
 // setup another route to listen on /categories
 app.get("/categories", function(req,res){
-    res.send("TODO: get all posts within the categories.json file");
+    //res.send("TODO: get all posts within the categories.json file");
+    blogService.getCategories().then((data)=>{
+        res.json(data)
+    }).catch((error)=>{
+        console.log(error)
+        //res.status(404).send("Error!")
+        res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+    })
 });
 
 // setup another route to display 404
@@ -46,4 +67,11 @@ app.use(function(req,res){
 });
 
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+blogService.initialize()
+.then(()=>{
+    //console.log()
+    app.listen(HTTP_PORT, onHttpStart);
+})
+.catch(err=>{
+    console.log(err);
+})
