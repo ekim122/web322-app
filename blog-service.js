fc508a4940534bbc083900e966cfad67060b3224
ekim122 <ekim122@myseneca.ca>
2022-03-18 00:03:31 -0400

    reset all exports with empty reject()

--- blog-service.js
+++ blog-service.js
@@ -1,34 +1,13 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-var posts = [];
-var categories = [];
+// var posts = [];
+// var categories = [];
 
 
 //initialize blog by reading/loading files
 module.exports.initialize = function(){
     return new Promise (function(resolve,reject){
-        fs.readFile('./data/posts.json', 'utf8', (err, data) => {
-            if (err){
-                console.log("Unable to read posts file")
-                reject(err)
-            }else{
-                posts = JSON.parse(data)
-                console.log("Posts initialization successful")
-                //resolve(posts)
-            }
-        });
-
-        fs.readFile('./data/categories.json', 'utf8', (err, data) => {
-            if (err){
-                console.log("Unable to read categories file")
-                reject(err)
-            }else{
-                categories = JSON.parse(data)
-                console.log("Categories initialization successful")
-                //resolve(categories)
-            }
-        });
-        resolve(posts, categories)
+        reject();
     })
 };
 
@@ -36,11 +15,7 @@ module.exports.initialize = function(){
 // get all posts
 module.exports.getAllPosts = function(){
     return new Promise (function(resolve,reject){
-        if(posts.length === 0){
-            reject("No results from 'getAllPosts' returned")
-        }else{
-            resolve(posts)
-        }
+        reject();
     })
 };
 
@@ -48,17 +23,7 @@ module.exports.getAllPosts = function(){
 // get 'published = yes' posts only
 module.exports.getPublishedPosts=function(){
     return new Promise (function(resolve,reject){
-        var tempPosts = [];
-        if(posts.length === 0){
-            reject("No results from 'getPublishedPosts' returned")
-        }else{
-            for (var i = 0; i < posts.length; i++){
-                if (posts[i].published == true){
-                    tempPosts.push(posts[i])
-                }
-            }
-            resolve(tempPosts)
-        }
+        reject();
     })
 };
 
@@ -66,11 +31,7 @@ module.exports.getPublishedPosts=function(){
 // get all categories
 module.exports.getCategories = function(){
     return new Promise (function(resolve,reject){
-        if(categories.length === 0){
-            reject("No results from 'getCategories' returned")
-        }else{
-            resolve(categories)
-        }
+        reject();
     })
 };
 
@@ -78,16 +39,7 @@ module.exports.getCategories = function(){
 // add post
 module.exports.addPost=function(postData){
     return new Promise (function(resolve,reject){
-        if (postData.published === undefined){
-            postData.published = false
-        }else{
-            postData.published = true
-        }
-        postData.id = posts.length + 1
-        var formatDate = new Date()
-        postData.postDate = formatDate.toISOString().split('T')[0]
-        posts.push(postData)
-        resolve(posts)
+        reject();
     })
 };
 
@@ -95,17 +47,7 @@ module.exports.addPost=function(postData){
 // get posts based on category num
 module.exports.getPostsByCategory=function(category){
     return new Promise (function(resolve,reject) {
-        var tempPost=[];
-        for (var i = 0; i < posts.length; i++){
-            if (posts[i].category == category){
-                tempPost.push(posts[i]);
-            }
-        }
-        if (tempPost.length === 0){
-            var error = "Category Result Not Found"
-            reject({message: error});
-        }
-        resolve(tempPost)
+        reject();
     })
 }
 
@@ -113,17 +55,7 @@ module.exports.getPostsByCategory=function(category){
 // get posts based on min date
 module.exports.getPostsByMinDate=function(minDate){
     return new Promise (function(resolve,reject) {
-        var tempPost=[]
-        for (var i = 0; i < posts.length; i++){
-            if(new Date(posts[i].postDate) >= new Date(minDate)){
-                tempPost.push(posts[i]);
-            }            
-        }
-        if (tempPost.length === 0){
-            var error = "Date Result Not Found"
-            reject({message: error});
-        }
-        resolve(tempPost)
+        reject();
     })
 }
 
@@ -131,17 +63,7 @@ module.exports.getPostsByMinDate=function(minDate){
 // get specific post id
 module.exports.getPostById=function(id){
     return new Promise (function(resolve,reject) {
-        var tempPost
-        for (var i = 0; i < posts.length; i++){
-            if (posts[i].id == id){
-                tempPost = posts[i]
-            }
-        }
-        if (!tempPost){
-            var error = "ID Result Not Found"
-            reject({message:error});
-        }
-        resolve(tempPost)
+        reject();
     })
 }
 
@@ -149,16 +71,6 @@ module.exports.getPostById=function(id){
 // get category by filtering posts
 module.exports.getPublishedPostsByCategory = function(category){
     return new Promise (function(resolve,reject){
-        var tempPosts = [];
-        if(posts.length === 0){
-            reject("No results from 'getPublishedPostsByCategory' returned")
-        }else{
-            for (var i = 0; i < posts.length; i++){
-                if (posts[i].published == true && posts[i].category == category){
-                    tempPosts.push(posts[i])
-                }
-            }
-            resolve(tempPosts)
-        }
+        reject();
     })
 };
