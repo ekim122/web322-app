16c23e379f299a5469ede7ff04e710bd15507ab6
ekim122 <ekim122@myseneca.ca>
2022-03-31 01:20:11 -0400

    implemented registerUser witin auth-service

--- auth-service.js
+++ auth-service.js
@@ -15,6 +15,8 @@ var userSchema = new Schema({
 
 let User; // to be defined on new connection (see initialize)
 
+
+//initialize
 module.exports.initialize = function () {
     return new Promise(function (resolve, reject) {
         let db = mongoose.createConnection("mongodb+srv://dbUser:dbUser2022@senecaweb.k3enu.mongodb.net/web322-app?retryWrites=true&w=majority");
@@ -28,3 +30,27 @@ module.exports.initialize = function () {
         });
     });
 };
+
+
+//register
+module.exports.registerUser = function(userData){
+    return new Promise(function(resolve, reject){
+        if (userData.password != userData.password2){
+            reject("Passwords do not match")
+        }
+        else{
+            let newUser = new User(userData)
+            newUser.save((err)=>{
+                if (err) {
+                    if (err.code == 11000){
+                        reject("User Name already taken")
+                    }
+                    reject("There was an error creating the user: " + err)
+                }
+                else{
+                    resolve()
+                }
+            })
+        }
+    })
+}
