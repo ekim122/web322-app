c28b656004fd192a5f191420d719ffef1eda6a17
ekim122 <ekim122@myseneca.ca>
2022-03-31 00:41:28 -0400

    implemented initialize within auth-server.js

--- auth-service.js
+++ auth-service.js
@@ -14,3 +14,17 @@ var userSchema = new Schema({
 })
 
 let User; // to be defined on new connection (see initialize)
+
+module.exports.initialize = function () {
+    return new Promise(function (resolve, reject) {
+        let db = mongoose.createConnection("mongodb+srv://dbUser:dbUser2022@senecaweb.k3enu.mongodb.net/web322-app?retryWrites=true&w=majority");
+
+        db.on('error', (err)=>{
+            reject(err); // reject the promise with the provided error
+        });
+        db.once('open', ()=>{
+           User = db.model("users", userSchema);
+           resolve();
+        });
+    });
+};
