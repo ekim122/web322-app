456cccc50123c3a7a5f042b545533bfc1889a992
ekim122 <ekim122@myseneca.ca>
2022-03-30 22:38:42 -0400

    added require and schema for mongoose

---
+++ auth-service.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var userSchema = new Schema({
+    "userName":{
+        "type":String,
+        "unique":true
+    },
+    "password":String,
+    "email":String,
+    "loginHistory":[{
+        "dateTime":Date,
+        "userAgent":String
+    }]
+})
